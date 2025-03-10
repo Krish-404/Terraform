81ae2e017e3a06d9573b0041fdab88f40530319e
Krish Gupta <krish.gupta@cloudambassadors.com>
2025-03-10 15:45:22 +0530

    Update mynetwork.tf

--- tfinfra/mynetwork.tf
+++ tfinfra/mynetwork.tf
@@ -18,3 +18,17 @@ allow {
     }
 source_ranges = ["0.0.0.0/0"]
 }
+# Create the mynet-vm-1 instance
+module "mynet-vm-1" {
+  source           = "./instance"
+  instance_name    = "mynet-vm-1"
+  instance_zone    = "Zone"
+  instance_network = google_compute_network.mynetwork.self_link
+}
+# Create the mynet-vm-2" instance
+module "mynet-vm-2" {
+  source           = "./instance"
+  instance_name    = "mynet-vm-2"
+  instance_zone    = "Zone 2"
+  instance_network = google_compute_network.mynetwork.self_link
+}
